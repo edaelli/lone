7733be264f83a1d8cfd894ff066d020302a805e4
Ed Daelli <edoardo.daelli@gmail.com>
2023-06-17 09:56:39 -0600

    more glp definitions

--- python3/lone/nvme/spec/commands/admin/get_log_page.py
+++ python3/lone/nvme/spec/commands/admin/get_log_page.py
@@ -1,4 +1,5 @@
 import ctypes
+import enum
 from lone.nvme.spec.structures import ADMINCommand, DataInCommon
 from lone.nvme.spec.commands.status_codes import NVMeStatusCode, status_codes
 
@@ -226,3 +227,400 @@ GetLogPageFirmwareSlotInfo = GetLogPageFactory('GetLogPageFirmwareSlotInfo',
 # Perform some checks to make sure it matches the spec
 assert ctypes.sizeof(GetLogPageFirmwareSlotInfoData) == 512
 assert GetLogPageFirmwareSlotInfoData.FRS3.offset == 24
+
+
+class GetLogPageChangedNamespaceListData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('CHANGED_NS', ctypes.c_uint32 * 1024),
+    ]
+
+
+GetLogPageChangedNamespaceList = GetLogPageFactory('GetLogPageChangedNamespaceList',
+                                                   0x04,
+                                                   GetLogPageChangedNamespaceListData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageChangedNamespaceListData) == 4096
+
+
+class GetLogPageCommandsSupportedAndEffectsData(DataInCommon):
+
+    class CommandsSupportedAndEffects(ctypes.Structure):
+        _pack_ = 1
+        _fields_ = [
+            ('CSUP', ctypes.c_uint32, 1),
+            ('LBCC', ctypes.c_uint32, 1),
+            ('NCC', ctypes.c_uint32, 1),
+            ('NIC', ctypes.c_uint32, 1),
+            ('CCC', ctypes.c_uint32, 1),
+            ('RSVD_0', ctypes.c_uint32, 11),
+            ('CSE', ctypes.c_uint32, 3),
+            ('UUID', ctypes.c_uint32, 1),
+            ('CSP', ctypes.c_uint32, 12),
+        ]
+
+    _pack_ = 1
+    _fields_ = [
+        ('ACS', CommandsSupportedAndEffects * 256),
+        ('IOCS', CommandsSupportedAndEffects * 256),
+        ('RSVD_0', ctypes.c_uint8 * 2048),
+    ]
+
+
+GetLogPageCommandsSupportedAndEffects = GetLogPageFactory(
+    'GetLogPageCommandsSupportedAndEffects',
+    0x05,
+    GetLogPageCommandsSupportedAndEffectsData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageCommandsSupportedAndEffectsData) == 4096
+
+
+class GetLogPageDeviceSelfTestData(DataInCommon):
+
+    class DeviceSelfTestResult(ctypes.Structure):
+
+        class Code(enum.Enum):
+            SHORT_DST_OPERATION = 1
+            EXT_DST_OPERATION = 2
+            VENDOR_SPECIFIC = 2
+
+        class Result(enum.Enum):
+            SUCCESS = 0
+            ABORTED_DST_CMD = 1
+            ABORTED_CTRL_LEVEL_RESET = 2
+            ABORTED_NS_REMOVAL = 3
+            ABORTED_NVM_FORMAT = 4
+            FATAL_ERROR = 5
+            UNKNOWN_FAILED_SEGMENT = 6
+            FAILED_SEGMENT = 7
+            ABORTED_UNKNOWN = 8
+            ABORTED_SANITIZE = 9
+
+        _pack_ = 1
+        _fields_ = [
+            ('Code', ctypes.c_uint8, 4),
+            ('Result', ctypes.c_uint8, 4),
+        ]
+
+        _pack_ = 1
+        _fields_ = [
+            ('Status', ctypes.c_uint8, 4),
+            ('Code', ctypes.c_uint8, 4),
+            ('SegmentNumber', ctypes.c_uint8),
+            ('ValidDiagInfo', ctypes.c_uint8),
+            ('RSVD_0', ctypes.c_uint8),
+            ('POH', ctypes.c_uint64),
+            ('NSID', ctypes.c_uint32),
+            ('FAIL_LBA', ctypes.c_uint64),
+            ('SCT', ctypes.c_uint8),
+            ('SC', ctypes.c_uint8),
+            ('VU', ctypes.c_uint16),
+        ]
+
+    _pack_ = 1
+    _fields_ = [
+        ('Operation', ctypes.c_uint8),
+        ('Completion', ctypes.c_uint8),
+        ('RSVD_0', ctypes.c_uint8 * 2),
+        ('Results', DeviceSelfTestResult * 20),
+    ]
+
+
+GetLogPageDeviceSelfTest = GetLogPageFactory(
+    'GetLogPageDeviceSelfTest',
+    0x06,
+    GetLogPageDeviceSelfTestData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageDeviceSelfTestData) == 564
+
+
+class GetLogPageTelemetryHostInitiatedData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint32 * 1024),
+    ]
+
+
+GetLogPageTelemetryHostInitiated = GetLogPageFactory(
+    'GetLogPageTelemetryHostInitiated',
+    0x07,
+    GetLogPageTelemetryHostInitiatedData)
+
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageTelemetryHostInitiatedData) == 4096
+
+
+class GetLogPageTelemetryControllerInitiatedData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageTelemetryControllerInitiated = GetLogPageFactory(
+    'GetLogPageTelemetryControllerInitiated',
+    0x08,
+    GetLogPageTelemetryControllerInitiatedData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageTelemetryControllerInitiatedData) == 4096
+
+
+class GetLogPageEnduranceGroupInformationData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageEnduranceGroupInformation = GetLogPageFactory(
+    'GetLogPageEnduranceGroupInformation',
+    0x09,
+    GetLogPageEnduranceGroupInformationData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageEnduranceGroupInformationData) == 4096
+
+
+class GetLogPagePredictableLatencyPerNVMSetData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPagePredictableLatencyPerNVMSet = GetLogPageFactory(
+    'GetLogPagePredictableLatencyPerNVMSet',
+    0x0a,
+    GetLogPagePredictableLatencyPerNVMSetData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPagePredictableLatencyPerNVMSetData) == 4096
+
+
+class GetLogPagePredictableLatencyEventAggregateData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPagePredictableLatencyEventAggregate = GetLogPageFactory(
+    'GetLogPagePredictableLatencyEventAggregate',
+    0x0b,
+    GetLogPagePredictableLatencyEventAggregateData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPagePredictableLatencyEventAggregateData) == 4096
+
+
+class GetLogPageAsymetricNamespaceAccessData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageAsymetricNamespaceAccess = GetLogPageFactory(
+    'GetLogPageAsymetricNamespaceAccess',
+    0x0c,
+    GetLogPageAsymetricNamespaceAccessData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageAsymetricNamespaceAccessData) == 4096
+
+
+class GetLogPagePersistentEventLogData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPagePersistentEventLog = GetLogPageFactory(
+    'GetLogPagePersistentEventLog',
+    0x0d,
+    GetLogPagePersistentEventLogData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPagePersistentEventLogData) == 4096
+
+
+class GetLogPageEnduranceGroupEventAggregateData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageEnduranceGroupEventAggregate = GetLogPageFactory(
+    'GetLogPageEnduranceGroupEventAggregate',
+    0x0f,
+    GetLogPageEnduranceGroupEventAggregateData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageEnduranceGroupEventAggregateData) == 4096
+
+
+class GetLogPageMediaUnitStatusData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageMediaUnitStatus = GetLogPageFactory(
+    'GetLogPageMediaUnitStatus',
+    0x10,
+    GetLogPageMediaUnitStatusData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageMediaUnitStatusData) == 4096
+
+
+class GetLogPageSupportedCapacityConfigurationListData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageSupportedCapacityConfigurationList = GetLogPageFactory(
+    'GetLogPageSupportedCapacityConfigurationList',
+    0x11,
+    GetLogPageSupportedCapacityConfigurationListData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageSupportedCapacityConfigurationListData) == 4096
+
+
+class GetLogPageFeatureIdentifiersSupportedAndEffectsData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageFeatureIdentifiersSupportedAndEffects = GetLogPageFactory(
+    'GetLogPageFeatureIdentifiersSupportedAndEffects',
+    0x12,
+    GetLogPageFeatureIdentifiersSupportedAndEffectsData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageFeatureIdentifiersSupportedAndEffectsData) == 4096
+
+
+class GetLogPageNVMeMICommandsSupportedAndEffectsData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageNVMeMICommandsSupportedAndEffects = GetLogPageFactory(
+    'GetLogPageNVMeMICommandsSupportedAndEffects',
+    0x13,
+    GetLogPageNVMeMICommandsSupportedAndEffectsData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageNVMeMICommandsSupportedAndEffectsData) == 4096
+
+
+class GetLogPageCommandAndFeatureLockdownData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageCommandAndFeatureLockdown = GetLogPageFactory(
+    'GetLogPageCommandAndFeatureLockdown',
+    0x14,
+    GetLogPageCommandAndFeatureLockdownData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageCommandAndFeatureLockdownData) == 4096
+
+
+class GetLogPageBootPartitionData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageBootPartition = GetLogPageFactory(
+    'GetLogPageBootPartition',
+    0x15,
+    GetLogPageBootPartitionData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageBootPartitionData) == 4096
+
+
+class GetLogPageRotationalMediaInformationData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageRotationalMediaInformation = GetLogPageFactory(
+    'GetLogPageRotationalMediaInformation',
+    0x16,
+    GetLogPageRotationalMediaInformationData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageRotationalMediaInformationData) == 4096
+
+
+class GetLogPageDiscoveryData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageDiscovery = GetLogPageFactory(
+    'GetLogPageDiscovery',
+    0x70,
+    GetLogPageDiscoveryData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageDiscoveryData) == 4096
+
+
+class GetLogPageReservationNotificationData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageReservationNotification = GetLogPageFactory(
+    'GetLogPageReservationNotification',
+    0x80,
+    GetLogPageReservationNotificationData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageReservationNotificationData) == 4096
+
+
+class GetLogPageSanitizeStatusData(DataInCommon):
+    _pack_ = 1
+    _fields_ = [
+        ('placeholder', ctypes.c_uint8 * 4096),
+    ]
+
+
+GetLogPageSanitizeStatus = GetLogPageFactory(
+    'GetLogPageSanitizeStatus',
+    0x81,
+    GetLogPageSanitizeStatusData)
+
+# Perform some checks to make sure it matches the spec
+assert ctypes.sizeof(GetLogPageSanitizeStatusData) == 4096
